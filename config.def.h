5c9eb54e8d38320100fc260083108d4b3d290020
mfkd <mattkfd@gmail.com>
2021-02-27 12:23:41 +0100

    add alacritty key definition

--- config.def.h
+++ config.def.h
@@ -57,6 +57,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", normbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *alacritty[]  = { "alacritty", NULL };
 static const char *browser[]  = { "google-chrome-stable", NULL };
 static const char *pbrowser[]  = { "google-chrome-stable", "--incognito", NULL };
 static const char *slock[]  = { "slock", NULL };
@@ -76,6 +77,7 @@ static Key keys[] = {
 	/* modifier                     key            function        argument */
 	{ MODKEY,               XK_p,            spawn,          {.v = dmenucmd } },
 	{ MODKEY,               XK_Return,       spawn,          {.v = termcmd } },
+	{ MODKEY|ControlMask,   XK_Return,       spawn,          {.v = alacritty } },
 	{ MODKEY,               XK_backslash,    spawn,          {.v = browser } },
 	{ MODKEY|ControlMask,   XK_backslash,    spawn,          {.v = pbrowser } },
 	{ MODKEY,               XK_bracketright, spawn,          {.v = clipmenu } },
