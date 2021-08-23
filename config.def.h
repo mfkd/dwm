2dfdaa0b6828c693a597447bd5cc6bb18aab5cfb
mfkd <mattkfd@gmail.com>
2021-08-23 21:18:59 +0200

    Keybinding overhaul
    Use buttery smooth keybindings for tags and substitute evictees with
    something sane.

--- config.def.h
+++ config.def.h
@@ -69,24 +69,24 @@ static const char *passmenu[] = { "passmenu", "-i", "-fn", dmenufont, "-nb", nor
 
 static Key keys[] = {
 	/* modifier                     key            function        argument */
-	{ MODKEY,               XK_p,            spawn,          {.v = dmenucmd } },
+	{ MODKEY,               XK_slash,        spawn,          {.v = dmenucmd } },
 	{ MODKEY,               XK_Return,       spawn,          {.v = termcmd } },
 	{ MODKEY|ControlMask,   XK_Return,       spawn,          {.v = kitty1 } },
 	{ MODKEY,               XK_backslash,    spawn,          {.v = browser } },
 	{ MODKEY|ControlMask,   XK_backslash,    spawn,          {.v = pbrowser } },
 	{ MODKEY,               XK_bracketright, spawn,          {.v = clipmenu } },
 	{ MODKEY|ControlMask,   XK_bracketright, spawn,          {.v = passmenu } },
-	{ MODKEY|ShiftMask,     XK_minus,      spawn,          {.v = volzero } },
+	{ MODKEY|ShiftMask,     XK_minus,        spawn,          {.v = volzero } },
 	{ MODKEY,               XK_minus,        spawn,          {.v = voldown } },
 	{ MODKEY,               XK_equal,        spawn,          {.v = volup } },
 	{ MODKEY,               XK_Delete,       spawn,          {.v = slock } },
 	{ MODKEY,               XK_b,            togglebar,      {0} },
-	{ MODKEY,               XK_j,            focusstack,     {.i = +1 } },
-	{ MODKEY,               XK_k,            focusstack,     {.i = -1 } },
-	{ MODKEY,               XK_i,            incnmaster,     {.i = +1 } },
-	{ MODKEY,               XK_d,            incnmaster,     {.i = -1 } },
-	{ MODKEY,               XK_h,            setmfact,       {.f = -0.05} },
-	{ MODKEY,               XK_l,            setmfact,       {.f = +0.05} },
+	{ MODKEY,               XK_comma,        focusstack,     {.i = +1 } },
+	{ MODKEY,               XK_period,       focusstack,     {.i = -1 } },
+	{ MODKEY|ControlMask,   XK_comma,        incnmaster,     {.i = +1 } },
+	{ MODKEY|ControlMask,   XK_period,       incnmaster,     {.i = -1 } },
+	{ MODKEY|ShiftMask,     XK_comma,        setmfact,       {.f = -0.05} },
+	{ MODKEY|ShiftMask,     XK_period,       setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,     XK_Return,       zoom,           {0} },
 	{ MODKEY,               XK_Tab,          view,           {0} },
 	{ MODKEY|ShiftMask,     XK_c,            killclient,     {0} },
@@ -96,17 +96,17 @@ static Key keys[] = {
 	{ MODKEY,               XK_space,        setlayout,      {0} },
 	{ MODKEY|ShiftMask,     XK_space,        togglefloating, {0} },
 	{ MODKEY|ShiftMask,     XK_0,            tag,            {.ui = ~0 } },
-	{ MODKEY,               XK_comma,        focusmon,       {.i = -1 } },
-	{ MODKEY,               XK_period,       focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,     XK_comma,        tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,     XK_period,       tagmon,         {.i = +1 } },
-	TAGKEYS(                XK_1,                            0)
-	TAGKEYS(                XK_2,                            1)
-	TAGKEYS(                XK_3,                            2)
-	TAGKEYS(                XK_4,                            3)
-	TAGKEYS(                XK_5,                            4)
-	TAGKEYS(                XK_6,                            5)
-	TAGKEYS(                XK_7,                            6)
+	{ MODKEY|ControlMask,   XK_3,            focusmon,       {.i = -1 } },
+	{ MODKEY|ControlMask,   XK_4,            focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,     XK_3,            tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,     XK_4,            tagmon,         {.i = +1 } },
+	TAGKEYS(                XK_i,                            0)
+	TAGKEYS(                XK_o,                            1)
+	TAGKEYS(                XK_p,                            2)
+	TAGKEYS(                XK_k,                            3)
+	TAGKEYS(                XK_l,                            4)
+	TAGKEYS(                XK_semicolon,                    5)
+	TAGKEYS(                XK_apostrophe,                   6)
 	TAGKEYS(                XK_8,                            7)
 	TAGKEYS(                XK_9,                            8)
 	{ MODKEY|ShiftMask,     XK_q,            quit,           {0} },
