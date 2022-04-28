ccb341ce69f624d538c5bc4571754383934e08ca
mfkd <mattkfd@gmail.com>
2022-04-28 19:19:13 +0200

    JetBrainsMono Nerd Font

--- config.def.h
+++ config.def.h
@@ -3,8 +3,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Source Code Pro:size=10", "monospace:size=10" };
-static const char dmenufont[]       = "Source Code Pro:size=10";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10", "monospace:size=10" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10";
 static const char normbordercolor[] = "#282828"; /* hard: #1d2021 / soft: #32302f  med: #282828 */
 static const char normbgcolor[]     = "#282828";
 static const char normfgcolor[]     = "#f9f5d7";
