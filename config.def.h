c05a171a3728d543c87e0d8259b5512c8d058796
mfkd <mattkfd@gmail.com>
2022-08-30 14:49:42 +0200

    Update JetBrainsMono Nerd font name

--- config.def.h
+++ config.def.h
@@ -3,8 +3,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10", "monospace:size=10" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font Mono:size=10", "monospace:size=10" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font Mono:size=10";
 static const char normbordercolor[] = "#0d1117";
 static const char normbgcolor[]     = "#0d1117";
 static const char normfgcolor[]     = "#b3b1ad";
